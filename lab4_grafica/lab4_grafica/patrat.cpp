d1314b759f5253024c56b72f0cefbbc1094abe62
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-03-16 11:18:31 +0200

    1.Sphere exercise

--- lab4_grafica/lab4_grafica/patrat.cpp
+++ lab4_grafica/lab4_grafica/patrat.cpp
@@ -8,6 +8,13 @@ Programul afiseaza un patrat pe care il translateaza pe axa x la apasarea sageti
 #include "glaux.h"
 
 static GLfloat x = 0;
+static GLfloat y = 0;
+static GLfloat z = 0;
+static GLfloat alpha = 0;
+
+GLUquadricObj* quadricObj = 0;
+
+
 
 void myInit() {
     glClearColor(1.0, 1.0, 1.0, 1.0);
@@ -23,26 +30,33 @@ void CALLBACK MutaDreapta()
     x = x + 10;
 }
 
+void CALLBACK rot_z_up(AUX_EVENTREC* event) {
+    alpha -= 10;
+}
+
+void CALLBACK rot_z_down(AUX_EVENTREC* event) {
+    alpha += 10;
+}
+
 void CALLBACK display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-
     glLoadIdentity();
-
     glTranslatef(x, 0, 0.0);
+    glRotatef(alpha, 1, 1, 1);
 
-    glBegin(GL_QUADS);
-    {
-        glColor3f(1.0, 0.0, 0.0);
-        glVertex2f(100, 100);
-        glColor3f(1.0, 1.0, 0.0);
-        glVertex2f(150.0, 100.0);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex2f(150.0, 150.0);
-        glColor3f(0.0, 1.0, 0.0);
-        glVertex2f(100.0, 150.0);
-    }
-    glEnd();
+    quadricObj = gluNewQuadric();
+    gluQuadricDrawStyle(quadricObj, GLU_LINE);
+    gluQuadricNormals(quadricObj, GLU_SMOOTH);
+    gluQuadricTexture(quadricObj, GL_TRUE);
+    gluQuadricOrientation(quadricObj, GLU_OUTSIDE);
+
+    // Sphere
+    glPushMatrix();
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glTranslatef(0, 10, 0);
+    gluSphere(quadricObj, 30, 100, 20);
+    glPopMatrix();
 
     glFlush();
 }
@@ -58,10 +72,10 @@ void CALLBACK myReshape(GLsizei w, GLsizei h)
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     if (w <= h) {
-        glOrtho(-160.0, 160.0, 160.0 * (GLfloat)h / (GLfloat)w, -160.0 * (GLfloat)h / (GLfloat)w, -10.0, 10.0);
+        glOrtho(-160.0, 160.0, 160.0 * (GLfloat)h / (GLfloat)w, -160.0 * (GLfloat)h / (GLfloat)w, -100.0, 100.0);
     }
     else {
-        glOrtho(-160.0 * (GLfloat)w / (GLfloat)h, 160.0 * (GLfloat)w / (GLfloat)h, -160.0, 160.0, -10.0, 10.0);
+        glOrtho(-160.0 * (GLfloat)w / (GLfloat)h, 160.0 * (GLfloat)w / (GLfloat)h, -160.0, 160.0, -100.0, 100.0);
     }
     glMatrixMode(GL_MODELVIEW);
 }
@@ -81,10 +95,13 @@ int main(int argc, char** argv)
 {
     auxInitDisplayMode(AUX_SINGLE | AUX_RGB);
     auxInitPosition(0, 0, 800, 600);
-    auxInitWindow("Un patrat");
+    auxInitWindow("Quadrics");
     myInit();
     auxKeyFunc(AUX_LEFT, MutaStanga);
     auxKeyFunc(AUX_RIGHT, MutaDreapta);
+
+    auxMouseFunc(AUX_LEFTBUTTON, AUX_MOUSEDOWN, rot_z_down);
+    auxMouseFunc(AUX_RIGHTBUTTON, AUX_MOUSEDOWN, rot_z_up);
 
     auxReshapeFunc(myReshape);
     auxMainLoop(display);
